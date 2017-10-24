7158bbe4bf142524a21ddf58394ce9ce0b9523a5
KevSan <kevinsanchez1989@gmail.com>
2017-10-24 09:49:33 -0400

    -Made the converter functionality modular

--- CryptoCurrencyConverter.py
+++ CryptoCurrencyConverter.py
@@ -10,36 +10,42 @@ def main_menu():
     action = input()
     return int(action)
 
+def input_check(input):
+    if (dict_crypt_curr.key_in_dict_check(input) == False):
+        print("Error Message")
+    else:
+        if (dict_crypt_curr.val_in_dict_check(input) == False):
+            print("Error Message")
+
+def converter():
+    print("Please enter first currency by it's name or it's ticker name")
+    first_currency = input()
+    input_check(first_currency)
+    print("Enter how many", first_currency, "you want to convert")
+    first_currency_qnty = int(input())
+    #place a check to make sure input is number
+
+    print("Pleas enter second currency by it's name or it's ticker name")
+    second_currency = input()
+    print(first_currency, "and", second_currency)
+
+    API1 = "https://chainz.cryptoid.info/" + first_currency + "/api.dws?q=ticker.usd"
+    API2 = "https://chainz.cryptoid.info/" + second_currency + "/api.dws?q=ticker.usd"
+
+    currencyPriceOne = requests.get(API1).json()
+    currencyPriceTwo = requests.get(API2).json()
+
+
+    quantityDifference = currencyPriceOne / currencyPriceTwo
+    result = quantityDifference * first_currency_qnty
+    print(first_currency_qnty, first_currency, " = ", result, second_currency)
+
+
 while(True):
 
     option = main_menu()
     if(option == 1):
-        #print(dict_crypt_curr.key_in_dict_check("LiteCoin"))
-        #print(dict_crypt_curr.key_in_dict_check("litecoin"))
-
-        print("Please enter first currency by it's name or it's ticker name")
-        currency1 = input()
-        print("Enter how many", currency1, "you want to convert")
-        currency1quantity = int(input())
-        print("Pleas enter second currency by it's name or it's ticker name")
-        currency2 = input()
-        print(currency1, "and", currency2)
-
-        API1 = "https://chainz.cryptoid.info/" + currency1 + "/api.dws?q=ticker.usd"
-        API2 = "https://chainz.cryptoid.info/" + currency2 + "/api.dws?q=ticker.usd"
-
-        currencyPriceOne = requests.get(API1).json()
-        currencyPriceTwo = requests.get(API2).json()
-
-        #print(currencyPriceOne, type(currencyPriceOne))
-
-
-        quantityDifference = currencyPriceOne / currencyPriceTwo
-        #print(quantityDifference)
-        result = quantityDifference * currency1quantity
-
-        print(currency1quantity, currency1, " = ", result, currency2)
-        #print(currencyPriceOne, "and", currencyPriceTwo)
+        converter()
 
     elif(option == 2):
         dict_crypt_curr.print_dict()
