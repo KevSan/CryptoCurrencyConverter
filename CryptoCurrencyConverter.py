2f51b7818dfcdd5073c068c50d4017a1ef337997
KevSan <kevinsanchez1989@gmail.com>
2017-10-28 23:47:39 -0400

    -Removed CaseInsensitiveDict for not working properly -Converted input to lower case

--- CryptoCurrencyConverter.py
+++ CryptoCurrencyConverter.py
@@ -12,15 +12,15 @@ def main_menu():
 
 def input_check(input):
     if (dict_crypt_curr.key_in_dict_check(input) == False):
-        print("Error Message")
-    else:
         if (dict_crypt_curr.val_in_dict_check(input) == False):
             print("Error Message")
 
 def converter():
     print("Please enter first currency by it's name or it's ticker name")
     first_currency = input()
-    input_check(first_currency)
+    if input_check(first_currency) == False:
+        return None
+    first_currency = dict_crypt_curr.dict_of_crypto[first_currency]
     print("Enter how many", first_currency, "you want to convert")
     first_currency_qnty = int(input())
     #place a check to make sure input is number
--- DictOfCryptoCurrencies.py
+++ DictOfCryptoCurrencies.py
@@ -1,18 +1,17 @@
-from requests.structures import CaseInsensitiveDict
 import requests
 
 
 summary_api = "https://chainz.cryptoid.info/explorer/api.dws?q=summary#"
 data_feed = requests.get(summary_api).json()
 
-cid_of_crypto = CaseInsensitiveDict({data_feed[key]['name'] : key for key in data_feed})
+dict_of_crypto = {data_feed[key]['name'].lower() : key for key in data_feed}
 
 def print_dict():
-    for key in cid_of_crypto:
-        print(key, ":", cid_of_crypto[key])
+    for key in dict_of_crypto:
+        print(key, ":", dict_of_crypto[key])
 
 def key_in_dict_check(key):
-    return(cid_of_crypto.__contains__(key))
+    return(dict_of_crypto.__contains__(key))
 
 def val_in_dict_check(val):
-    return(val in cid_of_crypto.values())
+    return(val in dict_of_crypto.values())
